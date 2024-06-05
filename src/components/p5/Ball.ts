c5aa409c8735a2224d3fc13113a865a1ec4b8af6
BuildTools <unconfigured@null.spigotmc.org>
2024-06-05 16:15:14 -0400

    Refactored to make boundary and ball shapes much simpler

--- src/components/p5/Ball.ts
+++ src/components/p5/Ball.ts
@@ -16,23 +16,18 @@ export default class Ball {
     position: Vector;
     velocity: Vector;
     acceleration: Vector;
-    shapeMap: Map<Shape, () => void>;
-    shapes: Shape[];
     drawer: Drawer;
     // #endregion
     /**
-     * The constructor for creating a ball. Initializes acceleration and velocity to 0,
-     * populates a shape map, and ensures that all the shapes are within the shape map.
+     * The constructor for creating a ball. Initializes acceleration and velocity to 0
      * @param p5 A p5 instance
-     * @param shape The shape of the ball. Must be in {@link shapes}
-     * @param shapes An array of {@link Shape} objects 
+     * @param shape The shape of the ball
      * @param size The size of the ball. Defaults to 10.
      * @param position The start position of the ball. Defaults to the center of the canvas.
      */
-    constructor(p5: P5CanvasInstance, shape: Shape, shapes: Shape[], size?: number, position?: Vector) {
+    constructor(p5: P5CanvasInstance, shape: Shape, size?: number, position?: Vector) {
         this.p5 = p5;
         this.shape = shape;
-        this.shapes = shapes;
 
         // Start position at center of canvas by default
         this.position = position || this.p5.createVector(p5.width / 2, p5.height / 2);
@@ -41,41 +36,13 @@ export default class Ball {
         this.acceleration = this.p5.createVector(0, 0);
         this.velocity = this.p5.createVector(0, 0);
 
-        this.shapeMap = new Map();
         this.drawer = new Drawer(this.p5);
-        this.populateShapeMap();
-        // Purposefully not catching error. It is a developer's error, and thus needs to be uncaught
-        this.ensureAllShapesInShapeMap();
-    }
-
-    private populateShapeMap(): void {
-        this.shapeMap.set("Square", () => this.drawer.drawSquare(this.position, this.size));
-        this.shapeMap.set("Circle", () => this.drawer.drawCircle(this.position, this.size));
-        this.shapeMap.set("Triangle", () => this.drawer.drawTriangle(this.position, this.size));
-    }
-
-
-    /**
-     * @throws {Error} Thrown if {@link shapes this.shapes} is undefined
-     */
-    private ensureAllShapesInShapeMap(): void {
-        if (!this.shapes) {
-            throw new Error("Shapes is not defined in Ball.ts. Cannot ensure all shapes are in shape map.");
-        }
-
-        this.shapes.forEach(validShape => {
-            if (!this.shapeMap.has(validShape)) {
-                throw new Error(validShape + " is not a defined shape in the shape map!");
-            }
-        });
-
     }
 
     public display(): void {
         this.p5.noStroke();
         this.p5.fill(0);
-        // ! is important as it tells compiler that the returned value cannot be undefined
-        this.shapeMap.get(this.shape)?.();
+        this.drawer.draw(this.shape, this.position, this.size);
     }
 
     // #region Movement
--- src/components/p5/Boundary.ts
+++ src/components/p5/Boundary.ts
@@ -13,7 +13,6 @@ export default class Boundary {
     acceleration: Vector;
     private p5: P5CanvasInstance;
     public shape: Shape;
-    private shapes: Shape[];
     private drawer: Drawer;
     /**
      * @default 100
@@ -25,67 +24,18 @@ export default class Boundary {
      * The position of the boundary.
      */
     public position: Vector;
-    /**
-     * @example this.shapeMap.get(this.shape)!();
-     */
-    private shapeMap: Map<Shape, () => void>;
-    /**
-     * @example this.containsMap.get(this.shape)!(point);
-     */
-    private containsMap: Map<Shape, (point: Vector) => boolean>;
 
     // #endregion
-    constructor(p5: P5CanvasInstance, shape: Shape, shapes: Shape[], size?: number, position?: Vector) {
+    constructor(p5: P5CanvasInstance, shape: Shape, size?: number, position?: Vector) {
         this.p5 = p5;
         this.shape = shape;
-
-        this.shapes = shapes;
 
         this.position = position || p5.createVector(0, 0);
         this.size = size || 100;
 
         this.velocity = this.p5.createVector(0, 0);
         this.acceleration = this.p5.createVector(0, 0);
-        this.shapeMap = new Map();
         this.drawer = new Drawer(this.p5);
-        this.containsMap = new Map();
-        this.populateShapeMap();
-        this.populateContainsMap();
-        // Purposefully not catching error. It is a developer's error, and thus needs to be uncaught
-        this.ensureAllShapesInShapeMap();
-    }
-
-    /**
-     * Used for initializing {@link shapeMap}
-     */
-    private populateShapeMap(): void {
-        this.shapeMap.set("Square", () => this.drawer.drawSquare(this.position, this.size));
-        this.shapeMap.set("Circle", () => this.drawer.drawCircle(this.position, this.size));
-        this.shapeMap.set("Triangle", () => this.drawer.drawTriangle(this.position, this.size));
-    }
-
-    /**
-     * Used for initializing {@link containsMap}
-     */
-    private populateContainsMap(): void {
-        this.containsMap.set("Square", (point: Vector) => this.pointInSquare(point));
-        this.containsMap.set("Circle", (point: Vector) => this.pointInCircle(point));
-    }
-
-    /**
-     * @throws {Error} Thrown if {@link shapes this.shapes} is undefined
-     */
-    private ensureAllShapesInShapeMap(): void {
-        if (!this.shapes) {
-            throw new Error("Shapes is not defined in Boundary.ts. Cannot ensure all shapes are in shape map.");
-        }
-
-        this.shapes.forEach(validShape => {
-            if (!this.shapeMap.has(validShape)) {
-                throw new Error(validShape + " is not a defined shape in the shape map!");
-            }
-        });
-
     }
 
     public createBoundary() {
@@ -93,41 +43,7 @@ export default class Boundary {
         this.p5.stroke(0);
         this.p5.strokeWeight(3);
 
-        // ! is important as it tells compiler that the returned value cannot be undefined
-        this.shapeMap.get(this.shape)!();
-    }
-
-    // #region Collisions
-
-    private pointInCircle(point: Vector): boolean {
-        /*console.log('point x: ', point.x)
-        console.log('point y: ', point.y)
-        console.log('x: ', this.position.x)
-        console.log('y: ', this.position.y)
-        console.log('Dist: ', this.p5.dist(point.x, point.y, this.position.x, this.position.y));
-        console.log('Size: ', this.size);
-        console.log('----------------');
-        */
-        return this.p5.dist(point.x, point.y, this.position.x, this.position.y) <= this.size;
-    }
-
-    private pointInSquare(point: Vector): boolean {
-        // return (
-        //     point.x >= this.position.x &&
-        //     point.x <= (this.position.x) + this.p5.width &&
-        //     point.y >= this.position.y &&
-        //     point.y <= (this.position.y) + this.p5.height
-        // );
-        return this.p5.dist(point.x, point.y, this.position.x, this.position.y) <= this.size;
-    }
-
-    /**
-     * Uses {@link containsMap this.containsMap}
-     * @param point A position Vector of a point
-     * @returns True if the object is in the shape
-     */
-    public contains(point: Vector): boolean {
-        return this.containsMap.get(this.shape)!(point);
+        this.drawer.draw(this.shape, this.position, this.size);
     }
     // #endregion
 }
