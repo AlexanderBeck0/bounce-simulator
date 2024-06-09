09efd43d15c607a88cbbad09e656bdabd2235b49
BuildTools <unconfigured@null.spigotmc.org>
2024-06-09 14:42:53 -0400

    Made Checkbox List more reusable and added more theming

--- src/components/UI/CheckboxList.tsx
+++ src/components/UI/CheckboxList.tsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 
 interface CheckboxListProps {
+    /**
+     * The text to show when the Checkbox List is closed
+     */
+    title: string;
+    /**
+     * The text to show when the Checkbox List is open. Defaults to the value within `title`
+     */
+    openTitle?: string;
     checkboxes: string[];
     onCheckboxChange: (checked: boolean, index: number) => void;
 }
@@ -23,14 +31,14 @@ export default function CheckboxList(props: CheckboxListProps): JSX.Element {
         <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-200">
             <input type="checkbox" className="peer" onClick={toggleDropdown} />
             <div className="collapse-title">
-                {isDropdownOpen ? 'Hide' : 'Show'} Forces
+                {isDropdownOpen ? (props.openTitle || props.title) : props.title}
             </div>
             <div className="collapse-content">
                 {props.checkboxes.map((checkbox: string, index: number) => {
                     return (
-                        <label key={index} className="label cursor-pointer flex items-center space-x-2 rounded-md hover:bg-gray-100 transition-colors duration-200">
+                        <label key={index} className="label cursor-pointer flex items-center space-x-2 rounded-md hover:bg-gray-200 active:bg-gray-300 transition-colors duration-200">
                             <span className="label-text text-gray-700">{checkbox}</span>
-                            <input type="checkbox" onChange={handleCheckboxChange(index)} className="checkbox h-5 w-5 border-gray-300 rounded" />
+                            <input type="checkbox" onChange={handleCheckboxChange(index)} className="checkbox h-5 w-5 border-gray-400 rounded transition-colors duration-200" />
                         </label>
                     )
                 })}
